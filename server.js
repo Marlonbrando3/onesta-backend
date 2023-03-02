97ddf29b6d8f87a6833df6230357727946263bab
Marek <marek@macbook-air-marek.home>
2023-03-02 11:56:03 +0100

    changed a cors link

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const newpropertyRouter = require('./routes/newproperty')
 require('dotenv/config');
 const app = express();
 app.use(cors({
-  origin:['http://localhost:3000'],
+  origin:['https://oneta.com.pl'],
   credentials:true
 }))
 
